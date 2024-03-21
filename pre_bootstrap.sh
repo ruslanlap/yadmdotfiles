9dfde2330ac48f9a97ea12e67e139ef02b301da7
ruslanlap <lapin@ucu.edu.ua>
2024-03-21 21:08:37 +0200

    initial

--- pre_bootstrap.sh
+++ pre_bootstrap.sh
@@ -9,6 +9,19 @@ curl -sfLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/
 # Give execute permissions to yadm
 chmod a+x ~/.local/bin/yadm
 
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions$ZSH/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH/custom/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+
+
 # Clone dotfiles repository
 ~/.local/bin/yadm clone --bootstrap -f https://github.com/ruslanlap/yadmdotfiles.git
 
